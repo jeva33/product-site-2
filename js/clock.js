89c530ab160653336c3a48d27e4750ccf1432bce
jeva33 <jonathaneva@gmail.com>
2016-12-16 12:30:48 -0800

    clock and opening animation

--- js/clock.js
+++ js/clock.js
@@ -1,15 +1,32 @@
-$(document).ready(function() {
-    var currentdate = new Date();
-    var l=0;
-    function callme()
-    {
-        currentdate = new Date();
-        $('.hour').html(currentdate.getHours()+12);
-        $('.min').html(currentdate.getMinutes());
-        $('.second').html(currentdate.getSeconds());
-        $('.data').css({'padding-left':l+'px'});
-        l=l+5;
-    }
-    window.setInterval(callme,1000);
+function updateClock ( )
+{
+    var currentTime = new Date ( );
+    var currentHours = currentTime.getHours ( );
+    var currentMinutes = currentTime.getMinutes ( );
+    var currentSeconds = currentTime.getSeconds ( );
 
+    // Pad the minutes and seconds with leading zeros, if required
+    currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+    currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+
+    // Choose either "AM" or "PM" as appropriate
+    var timeOfDay = ( currentHours < 12 ) ? "am" : "pm";
+
+    // Convert the hours component to 12-hour format if needed
+    currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+
+    // Convert an hours component of "0" to "12"
+    currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+
+    // Compose the string for display
+    var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+
+
+    $("#clock").html(currentTimeString);
+
+}
+
+$(document).ready(function()
+{
+    setInterval('updateClock()', 1000);
 });
---
+++ js/opening_timeline_ani.js
@@ -0,0 +1,13 @@
+$(document).ready( function() {
+
+    $('#clock').delay(1400).fadeIn(1000);
+    $('.filling-line').delay(1000).animate({width: "100%"}, 500);
+
+
+//$('.milestone-1').click(function(){
+//    $('.timeline').animate({
+//        scrollTop: $(".milestone-1-target").offset().top},
+//        'slow');
+//	});
+
+});
