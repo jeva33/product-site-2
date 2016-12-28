d83a5804f5ecce59da33f905ab94d639e67d9677
jonnavera <jonathan.eva@navera.com>
2016-12-28 15:24:44 -0800

    working toggle but not reloading with timeline

--- js/vert_timeline.js
+++ js/vert_timeline.js
@@ -1,11 +1,17 @@
 $(document).ready( function() {
 
-	$('.drill-down-arrow').click(function() {
 
+	$('.drill-down-arrow', this).click(function() {
+		
 		var isChecked = $(event.target).is('.drill-down-arrow');
 		    if(isChecked) { 
 		        $('.section-content', this).slideToggle('fast');
+		        $(this).removeClass('drill-down-arrow').addClass('arrow-close-icon');
+		    } 
+		    else {
+		    	$('.section-content', this).slideToggle();
+		        $(this).removeClass('arrow-close-icon').addClass('drill-down-arrow');
 		    }
 	})
 
-});
+});	
