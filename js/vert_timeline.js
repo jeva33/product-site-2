14f225feca0208022e7df1a751608eb4beea2c3d
jonnavera <jonathan.eva@navera.com>
2016-12-29 13:54:28 -0800

    iframe solution

--- js/vert_timeline.js
+++ js/vert_timeline.js
@@ -1,17 +1,17 @@
-$(document).ready( function() {
+// $(document).ready( function() {
 
 
-	$('.drill-down-arrow', this).click(function() {
+// 	$('.drill-down-arrow', this).click(function() {
 		
-		var isChecked = $(event.target).is('.drill-down-arrow');
-		    if(isChecked) { 
-		        $('.section-content', this).slideToggle('fast');
-		        $(this).removeClass('drill-down-arrow').addClass('arrow-close-icon');
-		    } 
-		    else {
-		    	$('.section-content', this).slideToggle();
-		        $(this).removeClass('arrow-close-icon').addClass('drill-down-arrow');
-		    }
-	})
+// 		var isChecked = $(event.target).is('.drill-down-arrow');
+// 		    if(isChecked) { 
+// 		        $('.section-content', this).slideToggle('fast');
+// 		        $(this).removeClass('drill-down-arrow').addClass('arrow-close-icon');
+// 		    } 
+// 		    else {
+// 		    	$('.section-content', this).slideToggle();
+// 		        $(this).removeClass('arrow-close-icon').addClass('drill-down-arrow');
+// 		    }
+// 	})
 
-});	
+// });	
