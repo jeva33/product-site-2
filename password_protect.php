5b3422dc230cd5412f8a8033be30bbcc009c9b43
jeva33 <jonathaneva@gmail.com>
2017-01-18 10:34:50 -0800

    login styling test

--- password_protect.php
+++ password_protect.php
@@ -109,7 +109,7 @@ function showLoginPasswordProtect($error_msg) {
   <form method="post">
     <h3>Please enter password to access this page</h3>
     <font color="red"><?php echo $error_msg; ?></font><br />
-<?php if (USE_USERNAME) echo 'Login:<br /><input type="input" name="access_login" /><br />Password:<br />'; ?>
+    <?php if (USE_USERNAME) echo 'Login:<br /><input type="input" name="access_login" /><br />Password:<br />'; ?>
     <input type="password" name="access_password" /><p></p><input type="submit" name="Submit" value="Submit" />
   </form>
   </div>
